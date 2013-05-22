bd2a788d270dbf037b036b3ef3a69bc6279ee964
mpagano <mpagano@cespi.unlp.edu.ar>
2013-05-22 14:39:55 -0300

    instalation version

--- docs/generacionInstalador/new_instalador.sh
+++ docs/generacionInstalador/new_instalador.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -fr aux
 mkdir aux
-VERSION="0.9.4"
+VERSION="0.9.5"
 cp -a ../../opac/ ../../intranet/ ../../includes aux/
 cd aux
 for i in $(find -name *.pl); do
@@ -24,4 +24,3 @@ echo $VERSION > meranunlp/VERSION
 tar -czvf meranunlp/intranetyopac.tar.gz opac/ intranet/ includes/
 tar -czvf meranunlp-v$VERSION.tar.gz meranunlp
 rm -fr meranunlp opac intranet includes
-
