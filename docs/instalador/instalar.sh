bd614b38be3179f2c4e7099015a9e629b568a908
Matías Pagano <mpagano@cespi.unlp.edu.ar>
2013-06-04 13:08:50 -0300

    apache2-mpm-prefork en instaladores de meran

--- docs/instalador/instalar.sh
+++ docs/instalador/instalar.sh
@@ -195,7 +195,7 @@ instalarDependencias()
         #Instalar paquetes
         #su
         apt-get update
-        apt-get install apache2 mysql-server libapache2-mod-perl2 libgd2-xpm libxpm4 htmldoc libaspell15 ntpdate -y
+        apt-get install apache2 apache2-mpm-prefork mysql-server libapache2-mod-perl2 libgd2-xpm libxpm4 htmldoc libaspell15 ntpdate -y
         #Configurar apache
         echo "Procederemos a habilitar en apache los modulos necesarios"
         a2enmod rewrite
