3e0f0c36252780e33e59794243ab71f30d33028a
Einar Lanfranco <einar@info.unlp.edu.ar>
2013-06-11 17:47:06 +0000

    Actualizando el instalador para admitir debian 7 en 32 bits.

--- docs/generacionInstalador/new_instalador.sh
+++ docs/generacionInstalador/new_instalador.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -fr aux
 mkdir aux
-VERSION="0.9.4"
+VERSION="0.9.5"
 cp -a ../../opac/ ../../intranet/ ../../includes aux/
 cd aux
 for i in $(find -name *.pl); do
@@ -17,11 +17,10 @@ for i in $(find -name *.pm); do
 	LANG=es_ES.iso88591 sed '/^$/d' /tmp/auxiliar > $i
 done;
 mkdir meranunlp
-cp ../../instalador/* meranunlp
+cp -r ../../instalador/* meranunlp
 rm meranunlp/intranetyopac.tar.gz
 cp ../README ../COPYING licencia.txt meranunlp/
 echo $VERSION > meranunlp/VERSION
 tar -czvf meranunlp/intranetyopac.tar.gz opac/ intranet/ includes/
 tar -czvf meranunlp-v$VERSION.tar.gz meranunlp
-rm -fr meranunlp opac intranet includes
-
+#rm -fr meranunlp opac intranet includes
