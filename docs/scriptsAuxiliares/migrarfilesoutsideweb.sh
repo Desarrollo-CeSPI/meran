11f638e35af382f2da1aacfb9a3898dfe4cb1ff0
J. Francisco Raposeiras <rapofran@gmail.com>
2013-06-25 16:01:55 -0300

    Update migrarfilesoutsideweb.sh
    Cambié el path de $ORIGEN asi está a la misma altura que $DESTINO.
    Agregué pictures-opac (fotos de socios)

--- docs/scriptsAuxiliares/migrarfilesoutsideweb.sh
+++ docs/scriptsAuxiliares/migrarfilesoutsideweb.sh
@@ -2,8 +2,9 @@
 if [ $# -eq 2] then
 	ORIGEN=$1
 	DESTINO=$2
-	cp -a $ORIGEN/opac-tmpl/uploads/portada $DESTINO/files/opac/uploads 
-	cp -a $ORIGEN/opac-tmpl/temas/DEO/imagenes $DESTINO/files/intranet/private-uploads/logos
+	cp -a $ORIGEN/opac/htdocs/opac-tmpl/uploads/portada $DESTINO/files/opac/uploads 
+	cp -a $ORIGEN/opac/htdocs/opac-tmpl/temas/DEO/imagenes $DESTINO/files/intranet/private-uploads/logos
+	cp -a $ORIGEN/intranet/htdocs/uploads/pictures-opac $DESTINO/files/intranet/private-uploads/pictures-opac
 else
 	echo "Necesito dos parametro: origen (/tmp/meran) y destino(/usr/share/meran)"
 fi
