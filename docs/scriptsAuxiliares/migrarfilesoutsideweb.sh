fa4d2c0a121d8f957bcc713bb43eeda7d21cbc3e
J. Francisco Raposeiras <rapofran@gmail.com>
2013-06-25 15:00:13 -0300

    Update migrarfilesoutsideweb.sh

--- docs/scriptsAuxiliares/migrarfilesoutsideweb.sh
+++ docs/scriptsAuxiliares/migrarfilesoutsideweb.sh
@@ -3,6 +3,7 @@ if [ $# -eq 2] then
 	ORIGEN=$1
 	DESTINO=$2
 	cp -a $ORIGEN/opac-tmpl/uploads/portada $DESTINO/files/opac/uploads 
+	cp -a $ORIGEN/opac-tmpl/temas/DEO/imagenes $DESTINO/files/intranet/private-uploads/logos
 else
 	echo "Necesito dos parametro: origen (/tmp/meran) y destino(/usr/share/meran)"
 fi
