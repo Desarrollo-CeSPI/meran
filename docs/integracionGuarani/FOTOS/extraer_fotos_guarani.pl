685fca4a5707197560de0643c23337f92c0c8f24
Matías Pagano <mpagano@cespi.unlp.edu.ar>
2014-05-12 15:32:46 -0300

    Fotos Guarani

---
+++ docs/integracionGuarani/FOTOS/extraer_fotos_guarani.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl -w
+#
+# Este script se usa para extraer las fotos de los usuarios
+
+# Carga los modulos requeridos
+use CGI::Session;
+use C4::Context;
+use Digest::MD5 qw(md5 md5_hex md5_base64);
+
+#
+# Abre el archivo cuyo nombre se mando como primer parámetro
+open(ARCHIVO,$ARGV[0]) || die("El archivo no abre");
+
+my $dbh = C4::Context->dbh;
+my $pictures_dir = C4::Context->config('picturesdir');
+
+my $line;
+my @fields;
+
+while ($line = <ARCHIVO>) {
+    # Arma un arreglo a partir de una linea del archivo
+    @fields = split(/\|/,$line);
+    my $nro_documento = $fields[2];
+    my $foto =  $fields[4];
+    my $file_name = md5_hex($foto).".jpg";
+    #print "nro_documento:: ".$nro_documento."\n";
+    #print "foto:: ".$foto."\n\n\n";
+
+
+    my $out = $dbh->prepare("SELECT * from usr_socio where nro_socio= ? ");
+    $out->execute($nro_documento);
+
+    if ($out->rows) {
+        my $datos=$out->fetchrow_hashref;
+		if (!$datos->{'foto'}){
+            if (length($foto) > 150 ){
+            #Hay foto o es muy chica? 
+                open(my $out, '>:raw', $pictures_dir.'/'.$file_name) or die "Unable to open: $!";
+                print $out pack('H*',$foto);
+                close($out);
+                print("UPDATE usr_socio SET foto= $file_name where id_socio=$datos->{'id_socio'} ;\n");
+            }
+		}
+    }
+}
+
