b299a22de0a79219e8451a092915959d0bd1f8d8
Carbone Miguel <mcarbone@cespi.unlp.edu.ar>
2014-05-14 18:53:08 -0300

    se reusa la busqueda en estantes virtuales, al momento de agreagr un registro al estanta

---
+++ intranet/cgi-bin/estantes/buscar.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+#
+# Meran - MERAN UNLP is a ILS (Integrated Library System) wich provides Catalog,
+# Circulation and User's Management. It's written in Perl, and uses Apache2
+# Web-Server, MySQL database and Sphinx 2 indexing.
+# Copyright (C) 2009-2013 Grupo de desarrollo de Meran CeSPI-UNLP
+#
+# This file is part of Meran.
+#
+# Meran is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Meran is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Meran.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+use strict;
+use CGI;
+use C4::Output;
+use C4::AR::Auth;
+
+use C4::AR::Estantes;
+
+my $input = new CGI;
+
+my $subestante_actual;
+my $padre_actual;
+
+ 
+my ($template, $session, $t_params) = get_template_and_user ({
+                                        template_name   => 'estantes/buscar.tmpl',
+                                        query       => $input,
+                                        type        => "intranet",
+                                        authnotrequired => 0,
+                                        flagsrequired => {  ui => 'ANY', 
+                                                            tipo_documento => 'ANY', 
+                                                            accion => 'CONSULTA', 
+                                                            entorno => 'undefined'},
+                                        debug => 1,
+                 });
+
+
+
+my %params_combo;
+$params_combo{'default'}          = C4::AR::Preferencias::getValorPreferencia("defaultTipoNivel3");
+$t_params->{'comboTipoDocumento'} = &C4::AR::Utilidades::generarComboTipoNivel3(\%params_combo);
+my $estantes_publicos             = C4::AR::Estantes::getListaEstantesPublicos();
+$t_params->{'subestante_actual'}  = $subestante_actual;
+$t_params->{'padre_actual'}       = $padre_actual;
+$t_params->{'cant_estantes'}      = @$estantes_publicos;
+$t_params->{'ESTANTES'}           = $estantes_publicos;
+
+
+C4::AR::Auth::output_html_with_http_headers($template, $t_params, $session);
