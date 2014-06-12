25ece6f413ab4185e995e96afbaed6f4aea4c7d5
Matías Pagano <mpagano@cespi.unlp.edu.ar>
2014-06-12 16:10:43 -0300

    Actualización de datos censales

---
+++ opac/cgi-bin/opac-actualizar_datos_censales.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+#
+# Meran - MERAN UNLP is a ILS (Integrated Library System) wich provides Catalog,
+# Circulation and User's Management. It's written in Perl, and uses Apache2
+# Web-Server, MySQL database and Sphinx 2 indexing.
+# Copyright (C) 2009-2013 Grupo de desarrollo de Meran CeSPI-UNLP
+#
+# This file is part of Meran.
+#
+# Meran is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Meran is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Meran.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+use strict;
+require Exporter;
+
+use C4::Output;  # contains gettemplate
+use C4::AR::Auth;
+use C4::Context;
+use CGI::Session;
+use C4::AR::Preferencias;
+
+my $input = new CGI;
+
+my ($template, $session, $t_params) =  C4::AR::Auth::get_template_and_user ({
+            template_name   => 'opac-main.tmpl',
+            query           => $input,
+            type            => "opac",
+            authnotrequired => 0,
+            flagsrequired   => {    ui              => 'ANY', 
+                                    tipo_documento  => 'ANY', 
+                                    accion          => 'CONSULTA', 
+                                    entorno         => 'undefined'},
+    });
+
+my $socio = C4::AR::Usuarios::getSocioInfoPorNroSocio(C4::AR::Auth::getSessionNroSocio());
+
+$t_params->{'socio'}        			= $socio;
+$t_params->{'needsDataValidation'}      = C4::AR::Usuarios::needsDataValidation($socio->getNro_socio);
+$t_params->{'partial_template'}         = "opac-modificar_datos_censales.inc";
+$t_params->{'content_title'}            = C4::AR::Filtros::i18n("Actualizaci&oacute;n de Datos");
+
+C4::AR::Auth::output_html_with_http_headers($template, $t_params, $session);
