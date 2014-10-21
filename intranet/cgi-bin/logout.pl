6ae08ecb379785998cc736d7eb09496856db46fb
Matías Pagano <mpagano@cespi.unlp.edu.ar>
2014-10-21 10:07:04 -0300

    logout

--- intranet/cgi-bin/logout.pl
+++ intranet/cgi-bin/logout.pl
@@ -24,27 +24,5 @@
 use strict;
 require Exporter;
 
-use C4::Output;  # contains gettemplate
 use C4::AR::Auth;
-use C4::Context;
-use CGI::Session;
-
-my $input=new CGI;
-
-my ($template, $session, $t_params) =  C4::AR::Auth::get_template_and_user ({
-            template_name   => 'auth.tmpl',
-            query       => $input,
-            type        => "intranet",
-            authnotrequired => 1,
-            flagsrequired   => {    ui => 'ANY', 
-                                    tipo_documento => 'ANY', 
-                                    accion => 'CONSULTA', 
-                                    entorno => 'undefined'},
-            loging_out      => 1,
-            change_password => 0,
-    });
-
-
-my ($session) = C4::AR::Auth::cerrarSesion();
-
-C4::AR::Auth::output_html_with_http_headers($template, $t_params, $session);
+C4::AR::Auth::_destruirSession();
