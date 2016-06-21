dbe70b05384836b5f82d18da9531b58227037540
Miguel Carbone <carbomiguel@gmail.com>
2016-06-21 16:27:46 -0300

    Agrego verificación para reseteo de password, siempre y cuando no este autenticando con LDAP

--- intranet/cgi-bin/usuarios/reales/detalleUsuario.pl
+++ intranet/cgi-bin/usuarios/reales/detalleUsuario.pl
@@ -84,5 +84,6 @@ $t_params->{'socio'}                    = $socio;
 $t_params->{'relativePicturesDir'}      = C4::Context->config("relativePicturesDir");
 $t_params->{'needsDataValidation'}      = C4::AR::Usuarios::needsDataValidation($nro_socio);
 $t_params->{'requisitoNecesario_pref'}  = C4::AR::Preferencias::getValorPreferencia('requisito_necesario');
+$t_params->{'ldapenabled'}              = C4::AR::Preferencias::getValorPreferencia('ldapenabled');
 
 C4::AR::Auth::output_html_with_http_headers($template, $t_params, $session);
