389304213a217d3fb888f0e8f357ff4b2554a5b2
Miguel Carbone <carbomiguel@gmail.com>
2017-06-15 16:40:54 -0300

    FIX getFileExtension

--- intranet/modules/C4/AR/Utilidades.pm
+++ intranet/modules/C4/AR/Utilidades.pm
@@ -4829,30 +4829,29 @@ sub isValidFile{
 sub getFileExtension{
 
     my ($file_path) = @_;
-    my $return_value = 1;
-    my $file_type;
+    # my $return_value = 1;
+    # my $file_type;
 
-    use File::LibMagic;
-    my $flm = File::LibMagic->new();
+    # use File::LibMagic;
+    # my $flm = File::LibMagic->new();
 
-    open(FILE, "<$file_path") or die "Can't open $file_path : $!\n";
-    close(FILE);
-
-    $file_type = $flm->checktype_filename($file_path);
+    # $file_type = $flm->checktype_filename($file_path);
     
-    my @extensiones_permitidas = C4::AR::UploadFile::getAllowedExtensionsArray();
-    my @nombreYextension=split('\.',$file_path);
+    # my @extensiones_permitidas = C4::AR::UploadFile::getAllowedExtensionsArray();
+    # my @nombreYextension=split('\.',$file_path);
 
-    my $size = scalar(@nombreYextension) - 1;
+    # my $size = scalar(@nombreYextension) - 1;
 
-    if (!( @nombreYextension[$size] =~ m/bmp|jpg|gif|png|jpeg|msword|docx|doc|odt|ods|pdf|xls|xlsx|zip/i) ) {
-      $return_value = 0;
-    } else {
-      $return_value = @nombreYextension[1];   
-    }
+    # if (!( @nombreYextension[$size] =~ m/bmp|jpg|gif|png|jpeg|msword|docx|doc|odt|ods|pdf|xls|xlsx|zip/i) ) {
+    #   $return_value = 0;
+    # } else {
+    #   $return_value = @nombreYextension[1];   
+    # }
 
 
-    return ($return_value);
+
+    my ($ext) = $file_path =~ /(\.[^.]+)$/;
+    return ($ext);
 }
 
 sub escapeURL{
