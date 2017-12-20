b5c17e5aa2096256df002ab30aa70d43f16bec7f
Miguel Carbone <carbomiguel@gmail.com>
2017-12-20 14:55:44 -0300

    Arreglados los errores de codificación en el correo para recuperar la contraseña

--- intranet/modules/C4/Output.pm
+++ intranet/modules/C4/Output.pm
@@ -49,7 +49,7 @@ sub gettemplate {
 
     my $htdocs;
     my $tema_opac   = C4::AR::Preferencias::getValorPreferencia('tema_opac_default') || C4::AR::Preferencias::getValorPreferencia('defaultUI');
-    my $tema_intra  = C4::Context->config('tema');; #para volver a tener temas, poner la linea de arriba 
+    my $tema_intra  = C4::Context->config('tema');; #para volver a tener temas, poner la linea de arriba
     my $temas       = C4::Context->config('temas');
     my $tema;
     my $type;
@@ -87,8 +87,9 @@ sub gettemplate {
                     RELATIVE        => 1,
                     EVAL_PERL       => 1,
                     LOAD_FILTERS    => [ $filter ],
+                    ENCODING        => 'utf8',
 #                   RELATIVE => 1,
-                    }); 
+                    });
 
     #se inicializa la hash de los parametros para el template
     my %params = ();
