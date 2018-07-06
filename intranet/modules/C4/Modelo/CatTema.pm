a1901f336cf1ab94b41fc93fe24b5ab785a7103f
Matías Pagano <matiaspagano@gmail.com>
2018-07-06 15:35:39 -0300

    Busqueda de Similares

--- intranet/modules/C4/Modelo/CatTema.pm
+++ intranet/modules/C4/Modelo/CatTema.pm
@@ -20,7 +20,8 @@ __PACKAGE__->meta->setup(
 use C4::Modelo::UsrRefCategoriaSocio;
 use C4::Modelo::CatTema::Manager;
 use Text::LevenshteinXS;
-    
+use String::Similarity;
+
 sub toString{
 	my ($self) = shift;
 
@@ -153,14 +154,24 @@ sub getAll{
 
     my $match = 0;
     if ($matchig_or_not){
+
         my @matched_array;
+        my $similarity_level =  C4::AR::Preferencias::getValorPreferencia("similarity");
         foreach my $each (@$ref_valores){
-          $match = ((distance($self_nombre,$each->getNombre)<=1));
-          if ($match){
-            push (@matched_array,$each);
+           my $similarity = similarity($self_nombre, $each->getNombre, $similarity_level);
+
+          if ($similarity gt $similarity_level){
+            my %table_data = {};
+            $table_data{"similarity"} = $similarity;
+            $table_data{"tabla_object"} = $each;
+            push (@matched_array, \%table_data);
           }
         }
+        #Ordenampos por similaridad
+        my @sorted_matched = sort { $b->{"similarity"} <=> $a->{"similarity"} } @matched_array;
+        my @matched_array = map { $_->{"tabla_object"} } @sorted_matched;
         return (scalar(@matched_array),\@matched_array);
+
     }
     else{
       return($ref_cant,$ref_valores);
